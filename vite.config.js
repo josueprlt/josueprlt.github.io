11e517490181541068ca095d6f62f4f5037bec04
josueprlt <148058543+josueprlt@users.noreply.github.com>
2025-01-06 12:31:20 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
     rollupOptions: {
       external: ['framer-motion'],
